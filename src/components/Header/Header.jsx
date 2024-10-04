61999fff4000a6f092781f0d493edddcb57f77fb
Olalakan Otele <lekanbiodun@gmail.com>
2024-10-04 07:24:35 +0100

    First commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
+          exercitationem illo perferendis praesentium, distinctio sed eius quia
+          temporibus rerum facere ex soluta qui debitis natus expedita dicta
+          odio quibusdam dolorum modi! A provident id, quasi odio optio tenetur
+          delectus earum!
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
